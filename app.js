3d2d72623cbad9360dc7d2396994167e80e48b25
Waqar Kabir <waqarkabir10@gmail.com>
2024-11-10 22:24:04 +0500

    Interactive theme switching implemented via JS

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener("click", function ()
+{
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+
+    const className = document.body.className;
+
+    className == "light-theme" ? this.textContent = "Dark" : this.textContent = "Light";
+
+    console.log('current class name: ' + className);
+});
